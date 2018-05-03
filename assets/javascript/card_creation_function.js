302225f4dcca95021f6ae4e2ad98591479772cc0
KenzieAnnC <35305283+KenzieAnnC@users.noreply.github.com>
2018-05-03 10:17:17 -0500

    adds jump to top button

--- assets/javascript/card_creation_function.js
+++ assets/javascript/card_creation_function.js
@@ -39,3 +39,17 @@ function searchGooglePlaces(midpoint) {
         }
     });    
  }
+
+ window.onscroll = function() {scrollFunction()};
+
+ function scrollFunction() {
+     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+         document.getElementById("myBtn").style.display = "block";
+     } else {
+         document.getElementById("myBtn").style.display = "none";
+     }
+ }
+ function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0; 
+}

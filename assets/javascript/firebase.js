1cce282c9127c650d9d8ecc919147e00e7ad6051
Nate <nathanperfetti@gmail.com>
2018-05-04 19:35:30 -0500

    Added dedicated firebase.js file for firebase configurations

---
+++ assets/javascript/firebase.js
@@ -0,0 +1,14 @@
+///////// ** Firebase Configuration **//////////////
+var config = {
+    apiKey: "AIzaSyBIaYZCktWCryF8cMhxK-XUNzIJibtquU0",
+    authDomain: "middlemeetup-a2868.firebaseapp.com",
+    databaseURL: "https://middlemeetup-a2868.firebaseio.com",
+    projectId: "middlemeetup-a2868",
+    storageBucket: "middlemeetup-a2868.appspot.com",
+    messagingSenderId: "482753535895"
+  };
+  firebase.initializeApp(config);
+  var database = firebase.database();
+  ///////// ** End Firebase Configuration **//////////////
+
+  

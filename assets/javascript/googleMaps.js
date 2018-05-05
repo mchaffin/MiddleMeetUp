230f023ca827fbbca39f412806908ad95496b175
Mark Chaffin <mark.chaffin@akimbosolutions.com>
2018-05-04 19:25:23 -0500

    fixing Google Places search for the CG suggestions well

--- assets/javascript/googleMaps.js
+++ assets/javascript/googleMaps.js
@@ -1,11 +1,30 @@
-// In the following example, markers appear when the user clicks on the map.
+// In the following code, markers appear when the user clicks on the map.
 // The markers are stored in an array.
 // The user can then click an option to hide, show or delete the markers.
-var map;
-var markers = [];
+//                                  // Global variables
+var map;                            // main map
+var markers = [];                   // marker array
+var origin = "";                    // initial origin location - geolocation sourced
+var destination = "";               // inital destination location - geolocation sourced
+var $middleMap = $("#middleMap");   // Jquery var for main map selector
+var place_id0 = "";                 // Google GeoPlace0
+var place_id1 = "";                 // Google GeoPlace1
+var midpointCoord = {};             // mid-point
 
+// Initialze map and wait for click
 function initMap() {
+    // Set intial map focus
     var twinCites = { lat: 44.96, lng: -93.17 };
+    // Virtual Directions Service
+    var directionsService = new google.maps.DirectionsService;
+    // Virtual Directions Renderer
+    var directionsDisplay = new google.maps.DirectionsRenderer; 
+    // main event listener - click handler
+    var onClickHandler = function (event) {
+        event.preventDefault();
+        calculateAndDisplayRoute(directionsService, directionsDisplay);
+        console.log("got here");
+    };
 
     map = new google.maps.Map(document.getElementById('middleMap'), {
         zoom: 12,
@@ -17,6 +36,10 @@ function initMap() {
     map.addListener('click', function(event) {
         addMarker(event.latLng);
     });
+    
+    // This event listener will call onClickHandler() when Run element ID is clicked.
+    document.getElementById('Run').addEventListener('click', onClickHandler);
+    // Add other click events here.
 
     // Adds a marker at the center of the map.
     addMarker(twinCites);
@@ -53,3 +76,29 @@ function deleteMarkers() {
     clearMarkers();
     markers = [];
 }
+
+/// NEW CODE HERE ///
+
+function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+    // Get origin and destination 
+    origin = document.getElementById('address1').value;
+    destination = document.getElementById('address2').value;
+
+    //Utilizes .route in directionsService to perform AJAX call 
+    directionsService.route({
+      // Save a call to DOM use origin and destination
+      origin: document.getElementById('address1').value,
+      destination: document.getElementById('address2').value,
+      travelMode: 'DRIVING'
+
+    }, function (response, status) {
+      if (status === 'OK') {
+        place_id0 = response.geocoded_waypoints["0"].place_id;
+        place_id1 = response.geocoded_waypoints["1"].place_id;
+        // Call mid-point function.
+        extractCoordinates(place_id0, place_id1);
+      } else {
+        window.alert('Directions request failed due to ' + status);
+      }
+    });
+  }
